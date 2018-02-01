16ed579d76d6ce4e0d534d841b7c90be05613107
Emily Welsch <emwelsch@gmail.com>
2018-02-01 17:58:30 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -51,8 +51,10 @@ def draw?(board)
 end
 
 def over?(board)
-  if draw?(board) == true || won?(board) == true || full?(board) == true
-    true
+  if draw?(board) == true || won?(board) == true
+    return true
+  else
+    return false
   end
 end
 
