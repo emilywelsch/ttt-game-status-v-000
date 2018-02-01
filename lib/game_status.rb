f2a8b3d671e641cf77a722c9155ee3efaa1c1734
Emily Welsch <emwelsch@gmail.com>
2018-02-01 17:51:11 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,8 +30,8 @@ def won?(board)
       return win_combination # return the X win_combination indexes that won.
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
         return win_combination # return the O win_combination indexes that won.
-      elsif position_taken?(board,index) ==
-
+    elsif position_taken?(board,index) == false
+        false
     else
       false
     end
